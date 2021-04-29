0a580ff1f933876567b4931657a033901b8b2dda
Ahmed Zaghrat <zaghrat.ahmed@gmail.com>
2021-04-29 03:55:33 +0200

    les bases PHP

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>$Title$</title>
+</head>
+<body>
+    <h1>Hello World!!</h1>
+    <!-- commentaire HTML -->
+    <?php
+        // simple line comment
+        /*
+         * Multiple lines comments
+         * another comment
+         */
+        echo "<h1>Hello World !!</h1>";
+
+        $personName = 'Ahmed';
+        echo "<p>Bonjour $personName</p>";
+    ?>
+</body>
+</html>
