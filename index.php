46bda8beff203e06c702257bd01f4f671555c853
Ahmed Zaghrat <zaghrat.ahmed@gmail.com>
2021-04-30 16:54:46 +0200

    tableaux en PHP

--- index.php
+++ index.php
@@ -3,21 +3,59 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>$Title$</title>
+    <title>Tableau en PHP</title>
 </head>
 <body>
-    <h1>Hello World!!</h1>
-    <!-- commentaire HTML -->
     <?php
-        // simple line comment
-        /*
-         * Multiple lines comments
-         * another comment
-         */
-        echo "<h1>Hello World !!</h1>";
+        echo "<pre>";
 
-        $personName = 'Ahmed';
-        echo "<p>Bonjour $personName</p>";
+        // Declarer un tableau
+    $tableau = ["Bonjour", 15, true, 58.15, "Ahmed", 45, false];
+
+
+    // Ajouter des valeurs
+    // a la fin (push)
+    array_push($tableau, 'Fin', 'Fin fin');
+
+
+    // Au debut (unshift)
+    array_unshift($tableau, 'debut');
+
+
+    // Supprimer des valeurs
+    // a la fin
+    array_pop($tableau);
+
+
+    // Au debut
+     array_shift($tableau);
+
+     var_dump($tableau);
+
+    // deviser un tableau en plusieurs parties de 2 valeurs
+    $tableau2 = array_chunk($tableau, 2, true);
+    var_dump($tableau2);
+
+    // melanger un tableau
+    shuffle($tableau);
+
+    $tableauMulti = [
+        0 => [
+        "nom" => "Ali",
+        "prenom"    => "Ahmed",
+        "age"   => 35,
+            ],
+        1 => [
+            "nom" => "Zaghrat",
+            "prenom"    => "Ahmed",
+            "age"   => 35,
+        ]
+    ];
+
+    var_dump($tableauMulti);
+
+    var_dump($tableauMulti[0]["nom"]);
+        echo "</pre>";
     ?>
 </body>
 </html>
